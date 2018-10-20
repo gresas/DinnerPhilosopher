9aa0ea3965ad7b186d4ff600b6295b08e96483ee
Guilherme <gbrondon@id.uff.br>
2018-10-20 19:10:49 -0300

    Added a LCD display to show the forks

--- src/Philosophers/Philosophers.ino
+++ src/Philosophers/Philosophers.ino
@@ -3,14 +3,22 @@
 
 #include <Interrupted.h>
 
+#include <LiquidCrystal.h>
+ 
+//Define os pinos que serão utilizados para ligação ao display
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
 Semaphore forks[MAX];
 static byte ids = 1;
 static byte leds[] = { LED1, LED2, LED3, LED4, LED5 };
+static byte buttonPin = 13;
+static bool state = false;
+static int vect[] = {1, 1, 1, 1, 1};
+
 
 void eating(byte id, byte weight) {
-
+  
 	digitalWrite(leds[id], LOW);
-
 	Tasks::delay(MAX * random(101, 301));
 
 	//digitalWrite(leds[id], LOW);
@@ -19,7 +27,6 @@ void eating(byte id, byte weight) {
 void thinking(byte id, byte weight) {
 
   digitalWrite(leds[id], HIGH);
-
   Tasks::delay(MAX * random(101, 301));
 
   // digitalWrite(leds[id], LOW);
@@ -34,13 +41,24 @@ public:
 	void loop() {
     thinking(_id, _w++);
   
-		forks[_id - 1].wait();
-		forks[_id].wait();
+    forks[_id - 1].wait();
+    forks[_id].wait();
+    vect[_id]--;
+    vect[_id - 1]--;
+    lcd.setCursor(2, 0);
+    char buff[12];
+    sprintf(buff, "|%d|%d|%d|%d|%d|", vect[0], vect[1], vect[2], vect[3], vect[4]);
+    lcd.print(buff);
+    
+    eating(_id, _w++);
 
-		eating(_id, _w++);
-
-		forks[_id - 1].signal();
-		forks[_id].signal();
+    forks[_id - 1].signal();
+    forks[_id].signal();  
+    vect[_id]++;
+    vect[_id - 1]++;
+    lcd.setCursor(2, 0);
+    sprintf(buff, "|%d|%d|%d|%d|%d|", vect[0], vect[1], vect[2], vect[3], vect[4]);
+    lcd.print(buff);
 	}
 
 private:
@@ -50,7 +68,10 @@ private:
 Philosopher philosophers[MAX-1];
 
 void setup() {
-
+  
+  //Define o número de colunas e linhas do LCD
+  lcd.begin(16, 2);
+  
 	Tasks::init();
 	Tasks::set_idle_handler(timer_sleep);
 	for (int i = 0; i < MAX; i++) {
@@ -59,10 +80,16 @@ void setup() {
 	}
 	for (int i = 0; i < MAX-1; i++)
 		Tasks::start(philosophers[i]);
+
+  lcd.setCursor(2, 0);
+  char buff[12];
+  sprintf(buff, "|%d|%d|%d|%d|%d|", vect[0], vect[1], vect[2], vect[3], vect[4]);
+  lcd.print(buff);
 }
 
 void loop() {
-	static byte weight;
+  
+  static byte weight;
   thinking(0, weight++);
   
   forks[0].wait();
@@ -71,5 +98,6 @@ void loop() {
   eating(0, weight++);
 
   forks[0].signal();
-  forks[MAX-1].signal();
+  forks[MAX-1].signal();  
+  
 }
