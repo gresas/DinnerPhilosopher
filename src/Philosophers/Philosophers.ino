c4c57906de18ca07fe2fda577c77f518f48e8a90
Guilherme <gbrondon@id.uff.br>
2018-10-23 13:50:57 -0300

    Added Button to control program flow and LCD to display information about the processes

--- src/Philosophers/Philosophers.ino
+++ src/Philosophers/Philosophers.ino
@@ -4,6 +4,12 @@
 #include <Interrupted.h>
 
 #include <LiquidCrystal.h>
+
+static byte buttonPin = 13;
+int buttonState;             // the current reading from the input pin
+int lastButtonState = LOW;   // the previous reading from the input pin
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 300;    // the debounce time; increase if the output flickers
  
 //Define os pinos que serão utilizados para ligação ao display
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
@@ -11,15 +17,20 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 Semaphore forks[MAX];
 static byte ids = 1;
 static byte leds[] = { LED1, LED2, LED3, LED4, LED5 };
-static byte buttonPin = 13;
 static bool state = false;
 static int vect[] = {1, 1, 1, 1, 1};
 
+void lcdPrint(){
+  lcd.setCursor(2, 0);
+  char buff[12];
+  sprintf(buff, "|%u|%u|%u|%u|%u|", forks[0]._count, forks[1]._count, forks[2]._count, forks[3]._count, forks[4]._count);
+  lcd.print(buff);
+}
 
 void eating(byte id, byte weight) {
   
 	digitalWrite(leds[id], LOW);
-	Tasks::delay(MAX * random(101, 301));
+	Tasks::delay(MAX * random(101, 501));
 
 	//digitalWrite(leds[id], LOW);
 }
@@ -27,7 +38,7 @@ void eating(byte id, byte weight) {
 void thinking(byte id, byte weight) {
 
   digitalWrite(leds[id], HIGH);
-  Tasks::delay(MAX * random(101, 301));
+  Tasks::delay(MAX * random(101, 501));
 
   // digitalWrite(leds[id], LOW);
 }
@@ -39,27 +50,36 @@ public:
 	}
 
 	void loop() {
-    thinking(_id, _w++);
+  int reading = digitalRead(buttonPin);
   
-    forks[_id - 1].wait();
-    forks[_id].wait();
-    vect[_id]--;
-    vect[_id - 1]--;
-    lcd.setCursor(2, 0);
-    char buff[12];
-    sprintf(buff, "|%d|%d|%d|%d|%d|", vect[0], vect[1], vect[2], vect[3], vect[4]);
-    lcd.print(buff);
-    
-    eating(_id, _w++);
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  }
 
-    forks[_id - 1].signal();
-    forks[_id].signal();  
-    vect[_id]++;
-    vect[_id - 1]++;
-    lcd.setCursor(2, 0);
-    sprintf(buff, "|%d|%d|%d|%d|%d|", vect[0], vect[1], vect[2], vect[3], vect[4]);
-    lcd.print(buff);
-	}
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (reading != buttonState) {
+      buttonState = reading;
+      if (buttonState == HIGH) {
+        thinking(_id, _w++);
+        
+        forks[_id - 1].wait();
+        lcdPrint();
+        forks[_id].wait();
+        lcdPrint();
+        
+        eating(_id, _w++);
+        
+        forks[_id - 1].signal();
+        lcdPrint();
+        forks[_id].signal();  
+        lcdPrint();
+      }
+    }
+  }
+  lastButtonState = reading;
+  lcdPrint();
+}
 
 private:
 	byte _id, _w;
@@ -71,6 +91,7 @@ void setup() {
   
   //Define o número de colunas e linhas do LCD
   lcd.begin(16, 2);
+  pinMode(buttonPin, INPUT);
   
 	Tasks::init();
 	Tasks::set_idle_handler(timer_sleep);
@@ -93,11 +114,14 @@ void loop() {
   thinking(0, weight++);
   
   forks[0].wait();
+  lcdPrint();
   forks[MAX-1].wait();
-
+  lcdPrint();
+  
   eating(0, weight++);
 
   forks[0].signal();
+  lcdPrint();
   forks[MAX-1].signal();  
-  
+  lcdPrint();
 }
